ccf86b02787bec4f942d7bfd0b52611d57e0190f
Vuelta7 <markvuelta07@gmail.com>
2025-02-14 01:16:51 +0800

    haha

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,6 +24,8 @@ class MyApp extends StatelessWidget {
 }
 
 class PDFQuestionGenerator extends StatefulWidget {
+  const PDFQuestionGenerator({super.key});
+
   @override
   _PDFQuestionGeneratorState createState() => _PDFQuestionGeneratorState();
 }
